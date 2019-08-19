5cdb07a5e262424d2837c87c31bd8a2bfec35f60
Lucas Lobato <lucklob94@gmail.com>
2019-08-19 20:24:32 -0300

    Atualizado

---
+++ Caes.java
@@ -0,0 +1,20 @@
+package trabalho;
+
+public class Caes {
+
+		String raca;
+		String cor;
+		float tamanho;
+		float peso;
+		int comer;
+		
+		
+	   void comida(){
+		   if(comer >=10) 
+			   System.out.println("Woof Woof");
+		   else
+			   System.out.println("FOME!!");	   
+		 
+	   }
+
+}
---
+++ Tester_caes.java
@@ -0,0 +1,19 @@
+package trabalho;
+
+public class Tester_caes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Caes c1 = new Caes();
+		c1.raca = "Golden Retriever";
+		c1.cor = "Bege";
+		c1.tamanho = (float) 1.2;
+		c1.peso = 34;
+		c1.comer = 5;
+		
+		c1.comida();
+	}
+
+	
+}
